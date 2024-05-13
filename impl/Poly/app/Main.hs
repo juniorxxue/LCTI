d9c929cc51b83cff2e61262189d37853a421fe26
Xu Xue <juniorxxue@gmail.com>
2024-05-13 21:10:07 +0800

    now powered with writer monad, but with some bugs

--- impl/Poly/app/Main.hs
+++ impl/Poly/app/Main.hs
@@ -1,8 +1,8 @@
-{-# LANGUAGE MultiWayIf, GADTs, LambdaCase #-}
+{-# LANGUAGE MultiWayIf #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Redundant multi-way if" #-}
 module Main where
-import Debug.Trace (trace)
+import Debug.Trace
 import Control.Monad.Writer
 
 type Log = [String]
@@ -102,12 +102,14 @@ shiftContext0 = shiftContext 0
 
 -- end shifting --
 
-lookupEnv :: Int -> Env -> Maybe Typ
-lookupEnv 0 (EBind ty _) = Just ty
+lookupEnv :: Int -> Env -> WriterT Log Maybe Typ
+lookupEnv 0 (EBind ty _) = do
+  tell ["[Lookup] " ++ show ty ++ " in Γ"]
+  return ty
 lookupEnv k (EBind _ env) = lookupEnv (k - 1) env
 lookupEnv k (ETyp env) = shiftTyp0 <$> lookupEnv k env
 lookupEnv k (ESol _ env) = shiftTyp0 <$> lookupEnv k env
-lookupEnv _ _ = Nothing
+lookupEnv _ _ = lift Nothing
 
 genericConsumer :: Trm -> Bool
 genericConsumer (Lit _) = True
@@ -152,30 +154,37 @@ closed senv (TForall t) = closed (STyp senv) t
 open :: SEnv -> Typ -> Bool
 open senv ty = not $ closed senv ty
 
-findSol' :: Env -> Int -> Maybe Typ
-findSol' EEmpty _ = Nothing
+findSol' :: Env -> Int -> WriterT Log Maybe Typ
+findSol' EEmpty _ = lift Nothing
 findSol' (EBind _ env) n = findSol' env n
-findSol' (ETyp env) n = if | n == 0 -> Nothing
+findSol' (ETyp env) n = if | n == 0 -> lift Nothing
                            | otherwise -> shiftTyp0 <$> findSol' env (n - 1)
-findSol' (ESol ty env) n = if | n == 0 -> Just ty
+findSol' (ESol ty env) n = if | n == 0 -> return ty
                               | otherwise -> shiftTyp0 <$> findSol' env (n - 1)
 
-findSol :: SEnv -> Int -> Maybe Typ
+findSol :: SEnv -> Int -> WriterT Log Maybe Typ
 findSol (Base env) n = findSol' env n
-findSol (STyp senv) n = if | n == 0 -> Nothing
+findSol (STyp senv) n = if | n == 0 -> lift Nothing
                            | otherwise -> shiftTyp0 <$> findSol senv (n - 1)
-findSol (SEx senv) n = if | n == 0 -> Nothing
+findSol (SEx senv) n = if | n == 0 -> lift Nothing
                           | otherwise -> shiftTyp0 <$> findSol senv (n - 1)
-findSol (SSol ty env) n = if | n == 0 -> Just ty
+findSol (SSol ty env) n = if | n == 0 -> return ty
                              | otherwise -> shiftTyp0 <$> findSol env (n - 1)
 
-fullInst :: SEnv -> Typ -> Typ
-fullInst _ TInt = TInt
-fullInst senv (TVar n) = case findSol senv n of
-  Just ty -> fullInst senv ty
-  Nothing -> TVar n
-fullInst senv (TArr t1 t2) = TArr (fullInst senv t1) (fullInst senv t2)
-fullInst senv (TForall t) = TForall $ fullInst (STyp senv) t
+fullInst :: SEnv -> Typ -> WriterT Log Maybe Typ
+fullInst _ TInt = return TInt
+fullInst senv (TVar n) = case runWriterT $ findSol senv n of
+  Just (ty, log') -> do
+    tell log'
+    fullInst senv ty
+  Nothing -> return $ TVar n
+fullInst senv (TArr t1 t2) = do
+  t1' <- fullInst senv t1
+  t2' <- fullInst senv t2
+  return $ TArr t1' t2'
+fullInst senv (TForall t) = do
+  t' <- fullInst (STyp senv) t
+  return $ TForall t'
 
 -- the side conditions in the sub ensures that
 -- when reaching the zero, it should be SEx
@@ -193,84 +202,137 @@ erase (STyp senv) = ETyp $ erase senv
 erase (SEx senv) = ESol TInt $ erase senv
 erase (SSol ty senv) = ESol ty $ erase senv
 
-sub :: SEnv -> Typ -> Context -> Maybe (SEnv, Typ)
-sub senv ty ctx | trace ("tracing -- sub " ++ show senv ++ " " ++ show ty ++ " " ++ show ctx) False = undefined
-sub senv TInt (CFullType TInt) = return (senv, TInt)
-sub senv tyA CEmpty | closed senv tyA = return (senv, newtyA)
-  where newtyA = fullInst senv tyA
-sub senv (TVar a) (CFullType (TVar b)) | isTyp senv a && a == b = return (senv, TVar a)
-sub senv (TVar a) (CFullType tyA) | isEx senv a && closed senv tyA = return (contextSubst tyA a senv, tyA)
+logSub :: SEnv -> Typ -> Context -> String
+logSub senv ty ctx = show senv ++ " ⊢ " ++ show ty ++ " <: " ++ show ctx ++ " ⊣ "
+
+sub :: SEnv -> Typ -> Context -> WriterT Log Maybe (SEnv, Typ)
+sub senv TInt (CFullType TInt) = do
+  tell ["[S-Int] " ++ logSub senv TInt (CFullType TInt)]
+  return (senv, TInt)
+sub senv tyA CEmpty | closed senv tyA = do
+  tell ["[S-Var] " ++ logSub senv tyA CEmpty]
+  newtyA <- censor indentAll $ fullInst senv tyA
+  return (senv, newtyA)
+sub senv (TVar a) (CFullType (TVar b)) | isTyp senv a && a == b = do
+  tell ["[S-Refl] " ++ logSub senv (TVar a) (CFullType (TVar b))]
+  return (senv, TVar a)
+sub senv (TVar a) (CFullType tyA) | isEx senv a && closed senv tyA = do
+  tell ["[S-Ex-L] " ++ logSub senv (TVar a) (CFullType tyA)]
+  return (contextSubst tyA a senv, tyA)
 sub senv (TVar a) (CFullType tyA) | closed senv tyA = do
-  tyB <- findSol senv a
-  (senv', tyA') <- sub senv tyB (CFullType tyA)
+  tyB <- censor indentAll $ findSol senv a
+  (senv', tyA') <- censor indentAll $ sub senv tyB (CFullType tyA)
+  tell ["[S-Sol-L] " ++ logSub senv (TVar a) (CFullType tyA)]
   return (senv', tyA')
-sub senv tyA (CFullType (TVar a)) | isEx senv a && closed senv tyA = return (contextSubst tyA a senv, tyA)
+sub senv tyA (CFullType (TVar a)) | isEx senv a && closed senv tyA = do
+  tell ["[S-Ex-R] " ++ logSub senv tyA (CFullType (TVar a))]
+  return (contextSubst tyA a senv, tyA)
 sub senv tyA (CFullType (TVar a)) | closed senv tyA = do
-  tyB <- findSol senv a
-  (senv', tyA') <- sub senv tyA (CFullType tyB)
+  tell ["[S-Sol-R] " ++ logSub senv tyA (CFullType (TVar a))]
+  tyB <- censor indentAll $ findSol senv a
+  (senv', tyA') <- censor indentAll $ sub senv tyA (CFullType tyB)
   return (senv', tyA')
 sub senv (TArr tyA tyB) (CFullType (TArr tyC tyD)) = do
-  (senv1, _) <- sub senv tyC (CFullType tyA)
-  (senv2, _) <- sub senv1 tyB (CFullType tyD)
+  tell ["[S-Arr] " ++ logSub senv (TArr tyA tyB) (CFullType (TArr tyC tyD))]
+  (senv1, _) <- censor indentAll $ sub senv tyC (CFullType tyA)
+  (senv2, _) <- censor indentAll $ sub senv1 tyB (CFullType tyD)
   return (senv2, TArr tyC tyD)
-sub senv (TArr tyA tyB) (CTerm e h) | closed senv tyA && closed senv tyB = do
-  _ <- infer (erase senv) (CFullType tyA) e
-  (senv', tyD) <- sub senv tyB h
+sub senv (TArr tyA tyB) (CTerm e h) | closed senv tyA = do
+  tell ["[S-Term-Closed] " ++ logSub senv (TArr tyA tyB) (CTerm e h)]
+  _ <- censor indentAll $ infer (erase senv) (CFullType tyA) e
+  (senv', tyD) <- censor indentAll $ sub senv tyB h
   return (senv', TArr tyA tyD)
 sub senv (TArr tyA tyB) (CTerm e h) | open senv tyA = do
-  tyC <- infer (erase senv) CEmpty e
-  (senv1, tyA') <- sub senv tyC (CFullType tyA)
-  (senv2, tyD) <- sub senv1 tyB h
+  tell ["[S-Term-Open] " ++ logSub senv (TArr tyA tyB) (CTerm e h)]
+  tyC <- censor indentAll $ infer (erase senv) CEmpty e
+  (senv1, tyA') <- censor indentAll $ sub senv tyC (CFullType tyA)
+  (senv2, tyD) <- censor indentAll $ sub senv1 tyB h
   return (senv2, TArr tyA' tyD)
 sub senv (TForall tyA) (CFullType (TForall tyB)) = do
-  (STyp senv', tyC) <- sub (STyp senv) tyA (CFullType tyB)
+  tell ["[S-Forall] " ++ logSub senv (TForall tyA) (CFullType (TForall tyB))]
+  (STyp senv', tyC) <- censor indentAll $ sub senv tyA (CFullType tyB)
   return (senv', TForall tyC)
 sub senv (TForall tyA) (CTerm e h) = do
-  (senv', tyB) <- sub (SEx senv) tyA (shiftContext0 (CTerm e h))
+  tell ["[S-Forall-L] " ++ logSub senv (TForall tyA) (CTerm e h)]
+  (senv', tyB) <- censor indentAll $ sub (SEx senv) tyA (shiftContext0 (CTerm e h))
   case senv' of
     STyp senv'' -> return (senv'', unshiftTyp0 tyB)
     SSol _ senv'' -> return (senv'', unshiftTyp0 tyB)
-    _ -> Nothing
+    _ -> lift Nothing
 sub senv (TForall tyA) (CTApp tyB h) = do
-  (SSol _ senv', tyC) <- sub (SSol tyB senv) tyA (shiftContext0 h)
+  tell ["[S-Forall-TApp] " ++ logSub senv (TForall tyA) (CTApp tyB h)]
+  (SSol _ senv', tyC) <- censor indentAll $ sub (SSol tyB senv) tyA (shiftContext0 h)
   return (senv', unshiftTyp0 tyC)
-sub _ _ _ = Nothing
+sub _ _ _ = lift Nothing
 
 nonEmptyContext :: Context -> Bool
 nonEmptyContext CEmpty = False
 nonEmptyContext _ = True
 
-infer :: Env -> Context -> Trm -> Maybe Typ
+logInfer :: Env -> Context -> Trm -> String
+logInfer env ctx tm = show env ++ " ⊢ " ++ show ctx ++ " => " ++ show tm ++ " => "
+
+indentAll :: [String] -> [String]
+indentAll = map ("  "++)
+
+infer :: Env -> Context -> Trm -> WriterT Log Maybe Typ
 infer a b c | trace ("tracing -- infer " ++ show a ++ " " ++ show b ++ " " ++ show c) False = undefined
-infer _ CEmpty (Lit _) = Just TInt
-infer e CEmpty (Var i) = lookupEnv i e
-infer e CEmpty (Ann tm tyA) = infer e (CFullType tyA) tm >> return tyA
-infer e h (App tm1 tm2) = infer e (CTerm tm2 h) tm1 >>= (\case {TArr _ ty12 -> return ty12; _ -> Nothing})
-infer e (CFullType (TArr tyA tyB)) (Abs tm) = do
-  tyC <- infer (EBind tyA e) (CFullType tyB) tm
+infer env CEmpty (Lit n) = do
+  tell ["[Ty-Int] " ++ logInfer env CEmpty (Lit n)]
+  return TInt
+infer env CEmpty (Var i) = do
+  tell ["[Ty-Var] " ++ logInfer env CEmpty (Var i)]
+  censor indentAll $ lookupEnv i env
+infer env CEmpty (Ann tm tyA) = do
+  tell ["[Ty-Ann] " ++ logInfer env CEmpty (Ann tm tyA)]
+  censor indentAll $ infer env (CFullType tyA) tm >> return tyA
+infer env h (App tm1 tm2) = do
+  tell ["[Ty-App] " ++ logInfer env h (App tm1 tm2)]
+  (TArr _ ty12) <- censor indentAll $ infer env (CTerm tm2 h) tm1
+  return ty12
+infer env (CFullType (TArr tyA tyB)) (Abs tm) = do
+  tell ["[Ty-Abs1] " ++ logInfer env (CFullType (TArr tyA tyB)) (Abs tm)]
+  tyC <- censor indentAll $ infer (EBind tyA env) (CFullType tyB) tm
   return $ TArr tyA tyC
-infer e (CTerm tm2 h) (Abs tm) = do
-  tyA <- infer e CEmpty tm2
-  tyB <- infer (EBind tyA e) (shiftContext0 h) tm
+infer env (CTerm tm2 h) (Abs tm) = do
+  tell ["[Ty-Abs2] " ++ logInfer env (CTerm tm2 h) (Abs tm)]
+  tyA <- censor indentAll $ infer env CEmpty tm2
+  tyB <- censor indentAll $ infer (EBind tyA env) (shiftContext0 h) tm
   return $ TArr tyA tyB
-infer e h g | genericConsumer g && nonEmptyContext h = do
-  tyA <- infer e CEmpty g
-  (_, tyB) <- sub (Base e) tyA h
+infer env h g | genericConsumer g && nonEmptyContext h = do
+  tell ["[Ty-Sub] " ++ logInfer env h g]
+  tyA <- censor indentAll $ infer env CEmpty g
+  (_, tyB) <- censor indentAll $ sub (Base env) tyA h
   return tyB
 infer e CEmpty (TAbs tm) = do
-  tyA <- infer (ETyp e) CEmpty tm
+  tell ["[Ty-TAbs] " ++ logInfer e CEmpty (TAbs tm)]
+  tyA <- censor indentAll $ infer (ETyp e) CEmpty tm
   return $ TForall tyA
-infer e h (TApp tm tyA) = infer e (CTApp tyA h) tm
-infer _ _ _ = Nothing
+infer e h (TApp tm tyA) = do
+  tell ["[Ty-TApp] " ++ logInfer e h (TApp tm tyA)]
+  censor indentAll $ infer e (CTApp tyA h) tm
+infer _ _ _ = lift Nothing
 
 main :: IO ()
 main = do
-  print idTyp
-  print idTrm
+  -- print idTyp
+  -- let results = runWriterT $ infer EEmpty CEmpty (App idTrm (Lit 1))
+  -- let results = runWriterT $ infer EEmpty CEmpty (Ann (Lit 1) TInt)
+  let results = runWriterT $ sub (Base EEmpty) (TForall (TArr (TVar 0) (TVar 0))) (CTerm (Lit 1) CEmpty)
+  case results of
+    Just (_, logs) -> mapM_ putStrLn logs
+    Nothing -> print "Nothing"
+
+  -- let results = runWriterT $ lookupEnv 1 (EBind TInt (EBind TInt EEmpty))
+  -- case results of
+  --   Just (ty, log) -> mapM_ putStrLn log
+  --   Nothing -> print "Nothing"
+
+  -- print idTrm
   -- print $ sub (Base EEmpty) (TForall (TArr (TVar 0) (TVar 0))) (CTerm (Lit 1) CEmpty)
   -- print $ sub (Base EEmpty) (TForall (TArr (TVar 0) (TVar 0))) (CTApp TInt (CTerm (Lit 1) CEmpty))
   -- print $ infer EEmpty CEmpty (App idTrm (Lit 1))
-  print $ infer EEmpty CEmpty idTrm
+  -- print $ infer EEmpty CEmpty idTrm
   -- print $ infer (ETyp EEmpty) CEmpty (Ann (Abs (Var 0)) (TArr (TVar 0) (TVar 0)))
   -- print $ infer EEmpty CEmpty idTrm
   -- print $ infer EEmpty CEmpty (Lit 1)
