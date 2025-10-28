4321bc696f0927b599277e06b13bf42325261dba
Xu Xue <juniorxxue@gmail.com>
2025-10-28 15:25:40 +0800

    pretty printer & :define command

--- impl_parser/Poly/app/Main.hs
+++ impl_parser/Poly/app/Main.hs
@@ -4,10 +4,11 @@ import Control.Monad.Writer
 import Examples (examplesList, Example(exampleName, exampleString), preEnvStrings)
 import Convert (convertNamedTerm, convertNamedEnv)
 import AST (NamedEnv(..))
+import qualified AST as AST
 import Infer
 import qualified Syntax as S
 import System.Console.Haskeline
-import qualified Data.Text as T
+import Data.Char (isSpace)
 
 preEnvNamed :: NamedEnv
 preEnvNamed = foldr (\(name, tyStr) env -> ETrm name (parseTyp tyStr) env) EEmpty preEnvStrings
@@ -19,37 +20,47 @@ preEnv = Convert.convertNamedEnv preEnvNamed
 main :: IO ()
 main = do
   putStrLn "Welcome to the type inference REPL of Fc. Type :q to quit."
-  putStrLn "Available commands: :tree, :env, :examples"
-  runInputT defaultSettings repl
-
-repl :: InputT IO ()
-repl = do
+  putStrLn "Available commands: :tree, :env, :examples, :define <name> : <type>"
+  runInputT defaultSettings (repl preEnvNamed preEnv)
+-- The REPL maintains a current named environment and its de Bruijn-converted form
+repl :: NamedEnv -> S.Env -> InputT IO ()
+repl envNamed env = do
   minput <- getInputLine "> "
   case minput of
-    Nothing   -> liftIO $ putStrLn "Bye."
-    Just ":q" -> liftIO $ putStrLn "Bye."
+    Nothing     -> liftIO $ putStrLn "Bye."
+    Just ":q"   -> liftIO $ putStrLn "Bye."
     Just input
-      | Just rest <- stripPrefix ":tree " input -> liftIO (showTree rest) >> repl
-      | Just rest <- stripPrefix ":env" input   -> liftIO showEnvNamed >> repl
-      | Just rest <- stripPrefix ":examples" input -> liftIO showExamples >> repl
-      | otherwise                               -> liftIO (showInfer input) >> repl
+      | Just rest <- stripPrefix ":tree " input     -> liftIO (showTree envNamed env rest) >> repl envNamed env
+      | Just _    <- stripPrefix ":env" input       -> liftIO (showEnvNamed envNamed) >> repl envNamed env
+      | Just _    <- stripPrefix ":examples" input  -> liftIO showExamples >> repl envNamed env
+      | Just rest <- stripPrefix ":define " input   -> do
+          let mDef = parseDefine rest
+          case mDef of
+            Left err -> outputStrLn err >> repl envNamed env
+            Right (name, tyNamed) -> do
+              let envNamed' = ETrm name tyNamed envNamed
+                  env' = convertNamedEnv envNamed'
+              -- Echo the defined type for convenience
+              outputStrLn (show tyNamed)
+              repl envNamed' env'
+      | otherwise                                 -> liftIO (showInfer envNamed env input) >> repl envNamed env
 
-showInfer :: String -> IO ()
-showInfer src = do
-  let term = convertNamedTerm preEnvNamed (parseTerm src)
-  case runWriterT $ infer preEnv S.CEmpty term of
+showInfer :: NamedEnv -> S.Env -> String -> IO ()
+showInfer envNamed env src = do
+  let term = convertNamedTerm envNamed (parseTerm src)
+  case runWriterT $ infer env S.CEmpty term of
     Just (ty, _) -> putStrLn (show ty)
     Nothing      -> putStrLn "failure"
 
-showTree :: String -> IO ()
-showTree src = do
-  let term = convertNamedTerm preEnvNamed (parseTerm src)
-  case runWriterT $ infer preEnv S.CEmpty term of
+showTree :: NamedEnv -> S.Env -> String -> IO ()
+showTree envNamed env src = do
+  let term = convertNamedTerm envNamed (parseTerm src)
+  case runWriterT $ infer env S.CEmpty term of
     Just (_, logs) -> putStr (unlines logs)
     Nothing        -> putStrLn "failure"
 
-showEnvNamed :: IO ()
-showEnvNamed = putStrLn (show preEnvNamed)
+showEnvNamed :: NamedEnv -> IO ()
+showEnvNamed envNamed = putStrLn (show envNamed)
 
 
 stripPrefix :: String -> String -> Maybe String
@@ -59,3 +70,18 @@ stripPrefix pre s =
 showExamples :: IO ()
 showExamples =
   mapM_ (\example -> putStrLn (exampleName example ++ ":\n " ++ exampleString example ++ "\n")) examplesList
+
+-- Parse a definition line of the form "<name> : <type>"
+parseDefine :: String -> Either String (String, AST.NamedTyp)
+parseDefine s =
+  case break (== ':') s of
+    (lhs, ':' : rhs) ->
+      let name  = trim lhs
+          tyStr = trim rhs
+      in if null name || null tyStr
+           then Left "Usage: :define <name> : <type>"
+           else Right (name, parseTyp tyStr)
+    _ -> Left "Usage: :define <name> : <type>"
+  where
+    trim = f . f
+      where f = reverse . dropWhile isSpace
